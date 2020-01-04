812dff0d4607e09946df440ebf191ce80ceb6e7c
mahongwei58 <mahongwei58@jd.com>
2020-01-04 18:20:58 +0800

    init

---
+++ main.go
@@ -0,0 +1,318 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/yanfeng1612/go-model/utils"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+)
+
+type Config struct {
+	ProjectName      string        `json:"projectName"`
+	ProjectAdminName string        `json:"projectAdminName"`
+	PackageName      string        `json:"packageName"`
+	ExportPath       string        `json:"exportPath"`
+	DataBaseName     string        `json:"dataBaseName"`
+	DatabaseIp       string        `json:"databaseIp"`
+	DatabasePort     int           `json:"databasePort"`
+	DatabaseUsername string        `json:"databaseUsername"`
+	DatabasePassword string        `json:"databasePassword"`
+	TableConfigList  []TableConfig `json:"tableConfigList"`
+}
+
+type TableConfig struct {
+	TableName       string   `json:"tableName"`
+	ShardingKey     string   `json:"shardingKey"`
+	DoubleWrite     int      `json:"doubleWrite"`
+	SingleQueryList []string `json:"singleQueryList"`
+	InListQuery     []string `json:"inListQuery"`
+	UpdateFieldList []string `json:"updateFieldList"`
+}
+
+const (
+	ConfigPath             = "D://go//src//github.com//yanfeng1612//go-model//json.txt" // 配置文件路径
+	TemplateRootPath       = "D://go//src//github.com//yanfeng1612//go-model//template" // 模板根路径
+	TemplateJavaRootPath   = TemplateRootPath + "//java"                                // java模板根路径
+	TemplateVueRootPath    = TemplateRootPath + "//vue"                                 // java模板根路径
+	PomPath                = TemplateJavaRootPath + "/utils/pom.vm"                     // pom路径
+	BasicResultPath        = TemplateJavaRootPath + "/utils/BasicResult.vm"             // BasicResultPath路径
+	GenericResultPath      = TemplateJavaRootPath + "/utils/GenericResult.vm"           // GenericResult路径
+	CodeEnumPath           = TemplateJavaRootPath + "/utils/CodeEnum.vm"                // CodeEnumPath路径
+	QueryPath              = TemplateJavaRootPath + "/utils/Query.vm"                   // Query路径
+	PageQueryPath          = TemplateJavaRootPath + "/utils/PageQuery.vm"               // PageQuery路径
+	IdPageQueryPath        = TemplateJavaRootPath + "/utils/IdPageQuery.vm"             // IdPageQuery路径
+	PagenationPath         = TemplateJavaRootPath + "/utils/Pagenation.vm"              // Pagenation路径
+	PageQueryWrapperPath   = TemplateJavaRootPath + "/utils/PageQueryWrapper.vm"        // PageQueryWrapper路径
+	ListResultPath         = TemplateJavaRootPath + "/utils/ListResult.vm"              // ListResult路径
+	PageListResultPath     = TemplateJavaRootPath + "/utils/PageListResult.vm"          // PageListResult路径
+	APIEmRequestStatusPath = TemplateJavaRootPath + "/utils/APIEmRequestStatus.vm"      // APIEmRequestStatus路径
+	APIMsgCodePath         = TemplateJavaRootPath + "/utils/APIMsgCode.vm"              // APIMsgCode路径
+	ResponsePath           = TemplateJavaRootPath + "/utils/Response.vm"                // Response路径
+	ResponseTemplatePath   = TemplateJavaRootPath + "/utils/ResponseTemplate.vm"        // ResponseTemplate路径
+	CodeConverterPath      = TemplateJavaRootPath + "/utils/CodeConverter.vm"           // CodeConverter路径
+	BootstrapPath          = TemplateJavaRootPath + "/utils/Bootstrap.vm"               // Bootstrap路径
+	ApplicationConfigPath  = TemplateJavaRootPath + "/utils/application.vm"             // application路径
+
+	PojoPath        = TemplateJavaRootPath + "/pojo.vm"        // pojo路径
+	PojoQueryPath   = TemplateJavaRootPath + "/pojoQuery.vm"   // pojoQuery路径
+	ControllerPath  = TemplateJavaRootPath + "/controller.vm"  // controller路径
+	ServicePath     = TemplateJavaRootPath + "/service.vm"     // service路径
+	ServiceImplPath = TemplateJavaRootPath + "/serviceImpl.vm" // serviceImpl路径
+	ManagerPath     = TemplateJavaRootPath + "/manager.vm"     // manager路径
+	ManagerImplPath = TemplateJavaRootPath + "/managerImpl.vm" // managerImpl路径
+	MapperPath      = TemplateJavaRootPath + "/mapper.vm"      // mapper路径
+	MapperXmlPath   = TemplateJavaRootPath + "/mapperXml.vm"   // mapperXml路径
+	vueListPath     = TemplateVueRootPath + "/list.vm"         // viewList路径
+	vueEditPath     = TemplateVueRootPath + "/edit.vm"         // viewEdit路径
+	vueDetailPath   = TemplateVueRootPath + "/detail.vm"       // viewDetail路径
+	vueIndexPath    = TemplateVueRootPath + "/index.vm"        // viewIndex路径
+	vueSidebarPath  = TemplateVueRootPath + "/sidebar.vm"      // viewSidebar路径
+)
+
+func main() {
+	jsonTxt, err := ioutil.ReadFile(ConfigPath)
+	if err != nil {
+		panic(err)
+	}
+	config := new(Config)
+	err = json.Unmarshal(jsonTxt, &config)
+	if err != nil {
+		panic(err)
+	}
+	jsonString, err := json.Marshal(config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(jsonString))
+
+	prepare(*config)
+
+	schema := utils.ParseSqlFromMySqlSchema("127.0.0.1", 3306, "root", "111111", "fund_cloud")
+
+	inListMap := make(map[string]int)
+	singleQueryMap := make(map[string]int)
+	updateFieldMap := make(map[string]int)
+	for _, t := range config.TableConfigList {
+		for _, c := range t.InListQuery {
+			inListMap[t.TableName+"_"+c] = 1
+		}
+		for _, c := range t.SingleQueryList {
+			singleQueryMap[t.TableName+"_"+c] = 1
+		}
+		for _, c := range t.UpdateFieldList {
+			updateFieldMap[t.TableName+"_"+c] = 1
+		}
+	}
+
+	for _, t := range schema.Tables {
+		for _, c := range t.Columns {
+			if _, ok := inListMap[t.Name+"_"+c.Name]; ok {
+				c.InListQuery = 1
+			} else {
+				c.InListQuery = 0
+			}
+			if _, ok := singleQueryMap[t.Name+"_"+c.Name]; ok {
+				c.SingleQuery = 1
+			} else {
+				c.SingleQuery = 0
+			}
+			if _, ok := updateFieldMap[t.Name+"_"+c.Name]; ok {
+				c.WhereUpdateField = 1
+			} else {
+				c.WhereUpdateField = 0
+			}
+		}
+	}
+
+	buildUtils(*config)
+	buildDynamic(*config, schema)
+}
+
+// 初始化 1.初始化文件目录
+func prepare(config Config) {
+	path := config.ExportPath + "/" + config.ProjectName + "/src/main/java/" + strings.ReplaceAll(config.PackageName, ".", "/")
+	err := os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		panic("创建项目文件夹失败!")
+	}
+	err = os.MkdirAll(path+"/utils/", os.ModePerm)
+	if err != nil {
+		panic("创建utils文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(path+"/pojo/", os.ModePerm)
+	if err != nil {
+		panic("创建pojo文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(path+"/controller/", os.ModePerm)
+	if err != nil {
+		panic("创建controller失败")
+	}
+	err = os.MkdirAll(path+"/service/", os.ModePerm)
+	if err != nil {
+		panic("创建service文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(path+"/service/impl/", os.ModePerm)
+	if err != nil {
+		panic("创建serviceImpl文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(path+"/manager/", os.ModePerm)
+	if err != nil {
+		panic("创建manager文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(path+"/manager/impl/", os.ModePerm)
+	if err != nil {
+		panic("创建managerImpl文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(path+"/dao/", os.ModePerm)
+	if err != nil {
+		panic("创建dao文件夹失败!" + err.Error())
+	}
+	err = os.MkdirAll(config.ExportPath+"/"+config.ProjectName+"/src/main/resources/mapper/auto/", os.ModePerm)
+	if err != nil {
+		panic("创建mapperXml文件夹失败!" + err.Error())
+	}
+
+	// 前端vue框架
+	err = os.MkdirAll(config.ExportPath+"/"+config.ProjectAdminName, os.ModePerm)
+	if err != nil {
+		panic("创建vue-admin失败")
+	}
+	err = os.MkdirAll(config.ExportPath+"/"+config.ProjectAdminName+"/src", os.ModePerm)
+	if err != nil {
+		panic("创建vue src失败")
+	}
+	err = utils.CopyDir(TemplateVueRootPath+"/static", config.ExportPath+config.ProjectAdminName)
+	if err != nil {
+		panic("创建admin静态资源文件失败")
+	}
+}
+
+func buildUtils(config Config) {
+	m := buildCommonMap(config)
+
+	prefix := config.ExportPath + "/" + config.ProjectName + "/src/main/java/" + strings.ReplaceAll(config.PackageName, ".", "/")
+	run(PomPath, config.ExportPath+"/"+config.ProjectName+"/pom.xml", m)
+	run(BootstrapPath, prefix+"/Bootstrap.java", m)
+	run(ApplicationConfigPath, config.ExportPath+"/"+config.ProjectName+"/src/main/resources/application.yml", m)
+	run(BasicResultPath, prefix+"/utils/BasicResult.java", m)
+	run(CodeEnumPath, prefix+"/utils/CodeEnum.java", m)
+	run(QueryPath, prefix+"/utils/Query.java", m)
+	run(GenericResultPath, prefix+"/utils/GenericResult.java", m)
+	run(PagenationPath, prefix+"/utils/Pagenation.java", m)
+	run(PageQueryPath, prefix+"/utils/PageQuery.java", m)
+	run(PageQueryWrapperPath, prefix+"/utils/PageQueryWrapper.java", m)
+	run(ListResultPath, prefix+"/utils/ListResult.java", m)
+	run(PageListResultPath, prefix+"/utils/PageListResult.java", m)
+	run(IdPageQueryPath, prefix+"/utils/IdPageQuery.java", m)
+	run(APIEmRequestStatusPath, prefix+"/utils/APIEmRequestStatus.java", m)
+	run(APIMsgCodePath, prefix+"/utils/APIMsgCode.java", m)
+	run(ResponsePath, prefix+"/utils/Response.java", m)
+	run(ResponseTemplatePath, prefix+"/utils/ResponseTemplate.java", m)
+	run(CodeConverterPath, prefix+"/utils/CodeConverter.java", m)
+}
+
+func run(templatePath, outputPath string, m map[string]interface{}) {
+	t := buildCommon(templatePath)
+	tmpFilePath := outputPath + "Tmp"
+	tmpFile, err := os.OpenFile(tmpFilePath, os.O_RDWR|os.O_TRUNC|os.O_CREATE, 066)
+	err = t.Execute(tmpFile, m)
+	if err != nil {
+		panic(err)
+	}
+	err = tmpFile.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	tmp, err := os.OpenFile(tmpFilePath, os.O_RDONLY, 066)
+	file, err := os.OpenFile(outputPath, os.O_RDWR|os.O_TRUNC|os.O_CREATE, 066)
+	defer file.Close()
+	reader := bufio.NewReader(tmp)
+	preLine := ""
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		newLine := strings.ReplaceAll(string(line), "&lt;", "<")
+		if strings.TrimSpace(newLine) == "" && preLine == "" {
+			continue
+		}
+		_, err = file.WriteString(newLine + "\r\n")
+		if err != nil {
+			panic(err)
+		}
+		preLine = strings.TrimSpace(newLine)
+	}
+	err = tmp.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = os.Remove(tmpFilePath)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func buildCommon(templatePath string) *template.Template {
+	textByte, err := ioutil.ReadFile(templatePath)
+	if err != nil {
+		panic(err)
+	}
+	text := string(textByte)
+	t := template.New("")
+	if t, err = t.Parse(text); err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func buildCommonMap(config Config) map[string]interface{} {
+	m := make(map[string]interface{})
+	m["author"] = "model-driven"
+	now := time.Now()
+	m["date"] = now.Format("2006-01-02")
+	m["config"] = config
+	return m
+}
+
+func buildDynamic(config Config, schema *utils.Schema) {
+	var pathPrefix = config.ExportPath + "/" + config.ProjectName + "/src/main/java/" + strings.ReplaceAll(config.PackageName, ".", "/")
+	for _, table := range schema.Tables {
+		m := buildCommonMap(config)
+		m["backendObject"] = table
+		m["columnSize"] = len(table.Columns)
+		run(PojoPath, pathPrefix+"/pojo/"+table.JavaBeanName+".java", m)
+		run(ControllerPath, pathPrefix+"/controller/"+table.JavaBeanName+"Controller.java", m)
+		run(PojoQueryPath, pathPrefix+"/pojo/"+table.JavaBeanName+"Query.java", m)
+		run(ServicePath, pathPrefix+"/service/"+table.JavaBeanName+"Service.java", m)
+		run(ServiceImplPath, pathPrefix+"/service/impl/"+table.JavaBeanName+"ServiceImpl.java", m)
+		run(ManagerPath, pathPrefix+"/manager/"+table.JavaBeanName+"Manager.java", m)
+		run(ManagerImplPath, pathPrefix+"/manager/impl/"+table.JavaBeanName+"ManagerImpl.java", m)
+		run(MapperPath, pathPrefix+"/dao/"+table.JavaBeanName+"Mapper.java", m)
+		run(MapperXmlPath, config.ExportPath+"/"+config.ProjectName+"/src/main/resources/mapper/auto/"+table.JavaBeanName+"Mapper.xml", m)
+
+		err := os.MkdirAll(config.ExportPath+"/"+config.ProjectAdminName+"/src/components/page/"+table.JavaBeanNameLower, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		run(vueListPath, config.ExportPath+"/"+config.ProjectAdminName+"/src/components/page/"+table.JavaBeanNameLower+"/list.vue", m)
+		//run(vueEditPath, config.ExportPath+"/"+config.ProjectAdminName+"/src/components/page/"+table.JavaBeanNameLower+"/edit.vue", m)
+		//run(vueDetailPath, config.ExportPath+"/"+config.ProjectAdminName+"/src/components/page/"+table.JavaBeanNameLower+"/detail.vue", m)
+	}
+	m := make(map[string]interface{})
+	m["tables"] = schema.Tables
+	m["l"] = "{{"
+	m["r"] = "}}"
+	run(vueIndexPath, config.ExportPath+"/"+config.ProjectAdminName+"/src/router/index.js", m)
+	run(vueSidebarPath, config.ExportPath+"/"+config.ProjectAdminName+"/src/components/common/Sidebar.vue", m)
+}
---
+++ utils/mysql.go
@@ -0,0 +1,170 @@
+package utils
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"strings"
+)
+
+type Schema struct {
+	Name   string
+	Tables []Table
+}
+
+type Table struct {
+	Name              string
+	JavaBeanName      string
+	JavaBeanNameLower string
+	Comment           string
+	Columns           []Column
+}
+
+type Column struct {
+	Name             string
+	Comment          string
+	ColumnType       string
+	JavaFieldName    string
+	JavaFieldNameUP  string
+	JavaFieldType    string
+	TableName        string
+	InListQuery      int
+	SingleQuery      int
+	RangeQuery       int
+	UpdateField      int
+	WhereUpdateField int
+}
+
+type ColumnType struct {
+	OriginType string
+}
+
+func ParseSql(txt string) Schema {
+	var schema Schema
+	schema.Name = "fund_cloud"
+	tables := make([]Table, 1)
+	lines := strings.Split(txt, "\r\n")
+	for _, line := range lines {
+		fmt.Println(line)
+		var table Table
+		if strings.Contains(line, " TABLE ") {
+			table.Name = line[strings.Index(line, "TABLE")+6 : strings.Index(line, "(")]
+			tables = append(tables, table)
+		}
+	}
+	schema.Tables = tables
+	return schema
+}
+
+func ParseSqlFromMySqlSchema(ip string, port int, username string, password string, databaseName string) *Schema {
+	db, _ := sql.Open("mysql", username+":"+password+"@/"+databaseName+"?charset=utf8")
+	rows, err := db.Query("SELECT TABLE_NAME,TABLE_COMMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = ?", databaseName)
+	if err != nil {
+		panic(err)
+	}
+
+	tableMap := make(map[string]*Table)
+	for rows.Next() {
+		var tableName, comment string
+		err = rows.Scan(&tableName, &comment)
+		if err != nil {
+			panic(err)
+		}
+		table := Table{
+			Name:              tableName,
+			JavaBeanName:      convertJavaBeanName(tableName),
+			JavaBeanNameLower: strings.ToLower(string(convertJavaBeanName(tableName)[0])) + convertJavaBeanName(tableName)[1:],
+			Comment:           comment,
+			Columns:           make([]Column, 0)}
+		tableMap[tableName] = &table
+	}
+
+	rows, err = db.Query("SELECT TABLE_NAME,COLUMN_NAME,COLUMN_COMMENT,DATA_TYPE FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = ?", databaseName)
+	if err != nil {
+		panic(err)
+	}
+
+	columns := make([]Column, 0)
+	for rows.Next() {
+		var tableName, name, comment, columnType string
+		err = rows.Scan(&tableName, &name, &comment, &columnType)
+		if err != nil {
+			panic(err)
+		}
+		updateField := 1
+		if name == "created_time" || name == "modified_time" {
+			updateField = 0
+		}
+		column := Column{
+			Name:            name,
+			Comment:         comment,
+			ColumnType:      columnType,
+			JavaFieldName:   convertJavaFieldName(name),
+			JavaFieldNameUP: convertJavaBeanName(name),
+			JavaFieldType:   convertJavaFieldType(columnType),
+			UpdateField:     updateField,
+			TableName:       tableName}
+		columns = append(columns, column)
+	}
+	for _, column := range columns {
+		table := tableMap[column.TableName]
+		if table == nil {
+			panic("cannot find table " + column.TableName)
+		}
+		table.Columns = append(table.Columns, column)
+	}
+	tables := make([]Table, 0)
+	for _, table := range tableMap {
+		tables = append(tables, *table)
+	}
+	schema := Schema{
+		Name:   databaseName,
+		Tables: tables}
+
+	return &schema
+}
+
+func convertJavaBeanName(tableName string) string {
+	result := strings.ToUpper(string(tableName[0]))
+	for i := 1; i < len(tableName); i++ {
+		if tableName[i] == '_' {
+			result += string(strings.ToUpper(string(tableName[i+1])))
+			i++
+		} else {
+			result += string(tableName[i])
+		}
+	}
+	return result
+}
+
+func convertJavaFieldName(tableName string) string {
+	result := ""
+	for i := 0; i < len(tableName); i++ {
+		if tableName[i] == '_' {
+			result += string(strings.ToUpper(string(tableName[i+1])))
+			i++
+		} else {
+			result += string(tableName[i])
+		}
+	}
+	return result
+}
+
+func convertJavaFieldType(t string) string {
+	result := ""
+	switch t {
+	case "bigint":
+		result = "Long"
+	case "int":
+		result = "Integer"
+	case "varchar":
+		result = "String"
+	case "date":
+		result = "Date"
+	case "datetime":
+		result = "Date"
+	case "timestamp":
+		result = "Date"
+	}
+	return result
+}
